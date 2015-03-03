72c269e4420fe6554eb87e36e76db15affa559ac
Matt Yohe <matt.yohe@gmail.com>
2015-03-02 21:36:41 -0500

    Update 1.2

--- ex-1.2.rkt
+++ ex-1.2.rkt
@@ -1,4 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname ex-1.2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
-(define num 3)
+(define num (+ 5 4) (- 2 (- 3 (+ 6 (/ 4 5)))))
+(define den (* 3 (- 6 2) (- 2 7)))
+(define rat (/ num den))
+rat
