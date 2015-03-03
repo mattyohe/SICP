01f6ccb5fa88475358b1728df321131bfdf75cc7
Matt Yohe <matt.yohe@gmail.com>
2015-03-02 22:40:53 -0500

    1.11

---
+++ ex-1.11.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex-1.11) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(define (f n)
+  (cond ((< n 3) n) 
+        (else (+ (f (- n 1))
+                 (* 2 (f (- n 2))) 
+                 (* 3 (f (- n 3)))))))
+
+(f 4)
+(f 5)
+(f 6)
+(f 7)
+(f 8)
+
